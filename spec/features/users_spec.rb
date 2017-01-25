9099ccd253013117a8cdc164d55eebcb92c7a308
Stefan Liute <stefanliute@gmail.com>
2017-01-25 20:56:24 +0000

    Challenge 19: Password confirmation

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -6,4 +6,17 @@ feature 'Signing up' do
     expect(page).to have_content 'Welcome johndoe@internet.com'
     expect(User.first.email).to eq('johndoe@internet.com')
   end
+
+  scenario 'with proper password confirmation' do
+    expect { sign_up(password_confirmation: 'otherpassword')}.not_to change(User, :count)
+  end
+
+  def sign_up(email: 'johndoe@internet.com', password: 'password', password_confirmation: 'password')
+    visit '/users/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+    click_button 'Sign up'
+  end
+
 end
